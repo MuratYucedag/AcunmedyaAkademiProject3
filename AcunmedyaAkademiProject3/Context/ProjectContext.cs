2fcf42f35d2e06cc506631b465bc150ce13ff0d2
MuratYucedag <yucedagmurat23@gmail.com>
2025-02-26 15:42:51 +0300

    Add project files.

---
+++ AcunmedyaAkademiProject3/Context/ProjectContext.cs
@@ -0,0 +1,17 @@
+﻿using AcunmedyaAkademiProject3.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunmedyaAkademiProject3.Context
+{
+    public class ProjectContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial catalog=AcunmedyaDb3;integrated security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+//trustservercertificate=true;
